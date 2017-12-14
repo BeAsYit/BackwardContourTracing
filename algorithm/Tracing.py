04f000f1d1aee2e7ea5047dd9ce2c1fb8dea315d
BeAsYit <beeasyit@gmail.com>
2017-12-15 01:43:12 +0200

    Completed algorithm for very simple photos

--- algorithm/Tracing.py
+++ algorithm/Tracing.py
@@ -6,7 +6,7 @@ class Tracing:
         self.backgroundPixels = []
         self.contourPixels = []
         self.image_params = self.image_params()
-        self.search = ((0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1))
+        self.search = ((0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1))
 
     def find_backgroundPixels(self):
         for height in range(self.image_params[0]):
@@ -15,10 +15,8 @@ class Tracing:
                                 self.image_params[2][width, height][2] > 50:
                     continue
                 self.backgroundPixels.append((width, height))
-        print("formed")
 
     def tracing(self):
-        print("Tracing")
         startP = activeP = self.find_start_pixel()
         cw_neighbourP = self.clockwise(activeP)
         counter_cw_neighbourP = self.counter_clockwise(activeP)
@@ -39,45 +37,39 @@ class Tracing:
                     activeP = contour_neighbourP
                 elif contour_neighbourP == endP:
                     break
-        print(self.contourPixels)
 
     def draw_countour(self):
-        img = Image.new('RGB', (self.image_params[1], self.image_params[0]))
+        img = Image.open("../data/1.jpg")
 
         for pixel in self.backgroundPixels:
-            img.putpixel(pixel, (255, 255, 255))
-
+            img.putpixel(pixel, (0, 0, 255))
         for pixel in self.contourPixels:
-            img.putpixel(pixel, (255, 0, 0))
+            img.putpixel(pixel, (0, 255, 0))
         img.show()
 
     def find_start_pixel(self):
-        print("finding start pixel")
         for height in range(self.image_params[0]):
             for width in range(self.image_params[1]):
                 if self.image_params[2][width, height][0] > 50 or self.image_params[2][width, height][1] > 50 or \
                                 self.image_params[2][width, height][2] > 50:
-                    print("found")
-                    print(width, height)
-                    return [width, height]
+                    return width, height
 
     def clockwise(self, activeP, previousP=False, index_previousP=0):
         if previousP:
             check = (index_previousP + 1) % 8
             new_search = self.search[check:] + self.search[:check]
             for i in new_search:
-                if [(activeP[0] + i[0]), (activeP[1] + i[1])] not in self.backgroundPixels:
-                    return [(activeP[0] + i[0]), (activeP[1] + i[1])]
+                if ((activeP[0] + i[0]), (activeP[1] + i[1])) not in self.backgroundPixels:
+                    return (activeP[0] + i[0]), (activeP[1] + i[1])
         else:
             for i in self.search:
-                if [(activeP[0] + i[0]), (activeP[1] + i[1])] not in self.backgroundPixels:
-                    return [(activeP[0] + i[0]), (activeP[1] + i[1])]
+                if ((activeP[0] + i[0]), (activeP[1] + i[1])) not in self.backgroundPixels:
+                    return (activeP[0] + i[0]), (activeP[1] + i[1])
 
     def counter_clockwise(self, activeP):
-        print("Counter clockwise")
         for i in reversed(self.search):
-            if [(activeP[0] + i[0]), (activeP[1] + i[1])] not in self.backgroundPixels:
-                return [(activeP[0] + i[0]), (activeP[1] + i[1])]
+            if ((activeP[0] + i[0]), (activeP[1] + i[1])) not in self.backgroundPixels:
+                return (activeP[0] + i[0]), (activeP[1] + i[1])
 
     def find_previous_index(self, previous, active):
         return self.search.index((previous[0] - active[0], previous[1] - active[1])) + 1
